d46147a62458824fad06e32a10b1db2b8a626cfc
Alfian Alfian <alfianpur@outlook.co.id>
2021-05-04 15:33:16 +0700

    Land

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import FeedPage from "./pages/FeedPage";
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 
 function App() {
   return (
     <div className="App">
-      <FeedPage/>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Signup}/>
+          <Route exact path="/login" component={Login}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
