2bccab725cc728082d41defaf341566b29442afe
Alfian Alfian <alfianpur@outlook.co.id>
2021-05-07 14:50:56 +0700

    New Boiler

---
+++ src/components/NotifPage/ContentNotification.jsx
@@ -0,0 +1,161 @@
+import React from "react"
+import "./style/contentNotification.css"
+import { Divider } from "@material-ui/core";
+import Pagination from '@material-ui/lab/Pagination';
+
+
+const ContentNotification = () => {
+    
+    return(
+        <>
+        <div className="main-container">
+            <div className="notif-container">
+                <div className="notif-wrapper">
+                    <div className="notif-header">
+                        <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/lion__RKncgdq5U.png" alt="content"/>
+                        <h4>Rafflesia Arnoldi</h4>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>1 hour ago</p>
+                    </div>
+                    <div className="sub-container">
+                        <div className="sub-wrapper">
+                            <div className="sub-header">
+                                <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/lion__RKncgdq5U.png" alt="sub-content"/>
+                                <h4>Black Swan</h4>
+                                <p>1 hour ago</p>
+                                <p>Personal</p>
+                            </div>
+                            <div className="sub-content">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, dolor? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate nisi eveniet illo? Maxime corporis porro possimus in, soluta vel provident!</p>
+                            </div>
+                        </div>   
+                    </div>
+                </div>
+            </div>
+            <Divider/>
+            <div className="notif-container">
+                <div className="notif-wrapper">
+                    <div className="notif-header">
+                        <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/lion__RKncgdq5U.png" alt="content"/>
+                        <h4>Rafflesia Arnoldi</h4>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>1 hour ago</p>
+                    </div>
+                    <div className="sub-container">
+                        <div className="sub-wrapper">
+                            <div className="sub-header">
+                                <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/lion__RKncgdq5U.png" alt="sub-content"/>
+                                <h4>Black Swan</h4>
+                                <p>10.00 AM</p>
+                                <p>Personal</p>
+                            </div>
+                            <div className="sub-content">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, dolor?</p>
+                            </div>
+                        </div>   
+                    </div>
+                </div>
+            </div>
+            <Divider/>
+            <div className="notif-container">
+                <div className="notif-wrapper">
+                    <div className="notif-header">
+                        <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/lion__RKncgdq5U.png" alt="content"/>
+                        <h4>Rafflesia Arnoldi</h4>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>1 hour ago</p>
+                    </div>
+                    <div className="sub-container">
+                        <div className="sub-wrapper">
+                            <div className="sub-header">
+                                <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/lion__RKncgdq5U.png" alt="sub-content"/>
+                                <h4>Black Swan</h4>
+                                <p>10.00 AM</p>
+                                <p>Personal</p>
+                            </div>
+                            <div className="sub-content">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, dolor?</p>
+                            </div>
+                        </div>   
+                    </div>
+                </div>
+            </div>
+            <Divider/>
+            <div className="notif-container">
+                <div className="notif-wrapper">
+                    <div className="notif-header">
+                        <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/lion__RKncgdq5U.png" alt="content"/>
+                        <h4>Rafflesia Arnoldi</h4>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>1 hour ago</p>
+                    </div>
+                    <div className="sub-container">
+                        <div className="sub-wrapper">
+                            <div className="sub-header">
+                                <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/lion__RKncgdq5U.png" alt="sub-content"/>
+                                <h4>Black Swan</h4>
+                                <p>10.00 AM</p>
+                                <p>Personal</p>
+                            </div>
+                            <div className="sub-content">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, dolor?</p>
+                            </div>
+                        </div>   
+                    </div>
+                </div>
+            </div>
+            <Divider/>
+            <div className="notif-container">
+                <div className="notif-wrapper">
+                    <div className="notif-header">
+                        <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/lion__RKncgdq5U.png" alt="content"/>
+                        <h4>Rafflesia Arnoldi</h4>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>1 hour ago</p>
+                    </div>
+                    <div className="sub-container">
+                        <div className="sub-wrapper">
+                            <div className="sub-header">
+                                <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/lion__RKncgdq5U.png" alt="sub-content"/>
+                                <h4>Black Swan</h4>
+                                <p>1 hour ago</p>
+                                <p>Personal</p>
+                            </div>
+                            <div className="sub-content">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, dolor?</p>
+                            </div>
+                        </div>   
+                    </div>
+                </div>
+            </div>
+            <Divider/>
+            <div className="notif-container">
+                <div className="notif-wrapper">
+                    <div className="notif-header">
+                        <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/lion__RKncgdq5U.png" alt="content"/>
+                        <h4>Rafflesia Arnoldi</h4>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>1 hour ago</p>
+                    </div>
+                    <div className="sub-container">
+                        <div className="sub-wrapper">
+                            <div className="sub-header">
+                                <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/lion__RKncgdq5U.png" alt="sub-content"/>
+                                <h4>Black Swan</h4>
+                                <p>10.00 AM</p>
+                                <p>Personal</p>
+                            </div>
+                            <div className="sub-content">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, dolor?</p>
+                            </div>
+                        </div>   
+                    </div>
+                </div>
+            </div>
+        </div>
+        {/* <Pagination count={5} defaultPage={1} color="primary" className="Pagination"/> */}
+        </>
+    )
+}
+
+export default ContentNotification;
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -7,6 +7,8 @@ import {
   useRouteMatch,
   useParams
 } from "react-router-dom";
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 import UserAct from '../components/UserProfile/UserAct';
 import UserBio from '../components/UserProfile/UserBio'
 import UserPost from '../components/UserProfile/UserPost';
@@ -17,69 +19,73 @@ const UserPage = () => {
   let match = useRouteMatch()
 
   return (
-    <div className="profile-container">
-      <div className="profile-wrapping">
-        {/* Start of Top Content */}
-        <div className="profile-top-content">
-          <img src="https://ik.imagekit.io/alfianpur/Final_Project/Rectangle_71_HTxe4aLXT.png" alt="Hero Profile Banner" />
-        </div>
-        {/* End of Top Content */}
-        {/* Start of Bottom Content */}
-        <div className="profile-bottom-content">
-          {/* Start of Left Content */}
-          <div className="profile-left-content">
-            <div className="relative">
-              <div className="profile-image">
-                <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/lion__RKncgdq5U.png" alt="Profile" />
-              </div>
-              <h1>Rafflesia Arnoldi</h1>
-              <div className="location">
-                <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/location_vBwnULTngQ.png" alt="loc" />
-                <p>Jakarta</p>
-              </div>
-            </div>
-            <button>
-              <p>Talk with anonymous</p>
-            </button>
+    <>
+      <Navbar/>
+      <div className="profile-container">
+        <div className="profile-wrapping">
+          {/* Start of Top Content */}
+          <div className="profile-top-content">
+            <img src="https://ik.imagekit.io/alfianpur/Final_Project/Rectangle_71_HTxe4aLXT.png" alt="Hero Profile Banner" />
           </div>
-          {/* End of Left Content */}
-          {/* Start of Right Content */}
-          <div className="profile-right-content">
-            {/* Start of Button Switch */}
-            <div className="switch-page-btn">
-              <div className="menu-item">
-                <Link to={`${match.url}`}>Profile</Link>
-                <div className="strip" />
+          {/* End of Top Content */}
+          {/* Start of Bottom Content */}
+          <div className="profile-bottom-content">
+            {/* Start of Left Content */}
+            <div className="profile-left-content">
+              <div className="relative">
+                <div className="profile-image">
+                  <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/lion__RKncgdq5U.png" alt="Profile" />
+                </div>
+                <h1>Rafflesia Arnoldi</h1>
+                <div className="location">
+                  <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/location_vBwnULTngQ.png" alt="loc" />
+                  <p>Jakarta</p>
+                </div>
               </div>
-              <div className="menu-item">
-                <Link to={`${match.url}/post`}>Post</Link>
-                <div className="strip" />
-              </div>
-              <div className="menu-item">
-                <Link to={`${match.url}/act`}>Activities</Link>
-                <div className="strip" />
-              </div>
+              <button>
+                <p>Talk with anonymous</p>
+              </button>
             </div>
-            {/* End of Button Switch */}
-            {/* Start Custome Insertion */}
-              <Switch>
-                <Route path={`${match.path}/post`}>
-                  <UserPost/>
-                </Route>
-                <Route path={`${match.path}/act`}>
-                  <UserAct/>
-                </Route>
-                <Route path={`${match.path}`}>
-                  <UserBio/>
-                </Route>
-              </Switch>
-            {/* End Custome Insertion */}
+            {/* End of Left Content */}
+            {/* Start of Right Content */}
+            <div className="profile-right-content">
+              {/* Start of Button Switch */}
+              <div className="switch-page-btn">
+                <div className="menu-item">
+                  <Link to={`${match.url}`}>Profile</Link>
+                  <div className="strip" />
+                </div>
+                <div className="menu-item">
+                  <Link to={`${match.url}/post`}>Post</Link>
+                  <div className="strip" />
+                </div>
+                <div className="menu-item">
+                  <Link to={`${match.url}/act`}>Activities</Link>
+                  <div className="strip" />
+                </div>
+              </div>
+              {/* End of Button Switch */}
+              {/* Start Custome Insertion */}
+                <Switch>
+                  <Route path={`${match.path}/post`}>
+                    <UserPost/>
+                  </Route>
+                  <Route path={`${match.path}/act`}>
+                    <UserAct/>
+                  </Route>
+                  <Route path={`${match.path}`}>
+                    <UserBio/>
+                  </Route>
+                </Switch>
+              {/* End Custome Insertion */}
+            </div>
+            {/* End of Right Content */}
           </div>
-          {/* End of Right Content */}
+          {/* End of Bottom Content */}
         </div>
-        {/* End of Bottom Content */}
       </div>
-    </div>
+      <Footer/>
+    </>
   )
 }
 
