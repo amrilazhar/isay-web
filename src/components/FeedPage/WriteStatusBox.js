14be70e206d4484f77ca6f5b3a353d0d9f0f65b5
Alfian Alfian <alfianpur@outlook.co.id>
2021-05-24 12:10:18 +0700

    handling media upload feed page

--- src/components/FeedPage/WriteStatusBox.js
+++ src/components/FeedPage/WriteStatusBox.js
@@ -12,7 +12,7 @@ const WriteStatusBox = () => {
   })
 
   const uploadFile = (e) => {
-    // e.preventDefault()
+    e.preventDefault()
     setFiles([...files, e.target.files[0]])
   }
 
@@ -38,14 +38,27 @@ const WriteStatusBox = () => {
     }
   }
 
-  return (
-    <div className="status-submit">
-      <div className="user-active-profile">
-        <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/lion__RKncgdq5U.png" alt="User" />
-      </div>
-      <form>
-        <textarea wrap="soft" type="text" name="status" id="status" placeholder="What do you feel about the world?" defaultValue={""} />
-        <div className="status-tools">
+  const [show, setShow] = useState(false)
+
+  const showModal = (e) => {
+    e.preventDefault()
+    if(show === false) {
+      setShow(true)
+    } else {
+      setShow(false)
+    }
+  }
+
+  const modal = () => {
+    if(show === false) {
+      return (<div></div>)
+    } else {
+    return (
+      <div id="resetModal" className="reset-modal">
+        <div className="reset-modal-content">
+          <button onClick={showModal} className="close">&times;</button>
+          <div>
+            <p>Share your moment to the world!</p>
           <input
             type="file"
             name="file"
@@ -55,6 +68,25 @@ const WriteStatusBox = () => {
             multiple
           />
           <label for="file"><strong>Image</strong></label>
+          </div>
+        </div>
+      </div>
+    )}
+  }
+
+
+
+  return (
+    <>
+    <div className="status-submit">
+      <div className="user-active-profile">
+        <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/lion__RKncgdq5U.png" alt="User" />
+      </div>
+      <form>
+        <textarea wrap="soft" type="text" name="status" id="status" placeholder="What do you feel about the world?" defaultValue={""} />
+        <div className="status-tools">
+          <button onClick={showModal}>Upload Image</button>
+          {modal()}
           <div className="interest-dropdown">
             <input className="choose" value={`${interest.interest}`}/>
             <div className="dropdown-content">
@@ -65,6 +97,7 @@ const WriteStatusBox = () => {
         </div>
       </form>
     </div>
+    </>
   )
 }
 
