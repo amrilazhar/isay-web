249452d1a01e6d1ffd8136dc47f181ef3f151a3a
Alfian Alfian <alfianpur@outlook.co.id>
2021-05-31 19:26:22 +0700

    handle change state of user page and also user another page

--- src/components/UserProfile/UserActMap.js
+++ src/components/UserProfile/UserActMap.js
@@ -1,12 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { history } from '../../helpers'
+import { alertActions } from '../../redux/actions'
+import FlashMessage from '../FlashMessage'
 
 const UserActMap = (fromAct) => {
 
   const userName = fromAct.userName
   const type = fromAct.type
   const ownerId = fromAct.ownerId
-  const statusId = fromAct.statusId
+  const valueOfStatus = fromAct.valueOfStatus
   const interest = fromAct.interest
   const statusOwner = fromAct.statusOwner
   const avatar = fromAct.avatar
@@ -18,12 +22,20 @@ const UserActMap = (fromAct) => {
   const actDate = fromAct.actDate
   const statusDate = fromAct.statusDate
 
+  const dispatch = useDispatch()
+  const alert = useSelector ((state) => state.alert)
+
   const singleStatus = () =>{
-    history.replace(`./user/${ownerId}`)
+    (valueOfStatus !== null)?
+    history.replace(`./user/${ownerId}`):
+    dispatch(alertActions.success('user not found, cause they are delete the message'));
   }
 
   return (
     <div className="isay-status-box">
+      {
+        alert.alert ? <FlashMessage/> : ""
+      }
       <div className="header-activity">
         <div className="header-line-one">
           <p>{userName}</p>
@@ -35,53 +47,58 @@ const UserActMap = (fromAct) => {
         <p>{actDate}</p>
       </div>
       <div onClick={singleStatus} style={{cursor: "pointer"}} className="isay-status-rec">
-        <div className="user-status">
-          <div className="upper-prop">
-            <div className="user-image">
-              <img src={avatar} alt="User" />
+        { valueOfStatus !== null ?
+          <>
+            <div className="user-status">
+              <div className="upper-prop">
+                <div className="user-image">
+                  <img src={avatar} alt="User" />
+                </div>
+                <div className="name-and-time">
+                  <h2>{statusOwner}</h2>
+                  <p>{statusDate}</p>
+                </div>
+                <div className="location">
+                  <p><FontAwesomeIcon icon={["fas", "map-marker-alt"]} size="1x" color="#4f4f4f"/>{location}</p>
+                </div>
+              </div>
+              <div className="lower-prop">
+                <p>{content}</p>
+                <div className="image-post">
+                  {
+                    (!media)? <div></div> :
+                    <>{
+                      media?.map(media =>(
+                        <div className="image-cont">
+                          <img src={`${media}`} alt="PostMage" />
+                        </div>
+                      ))
+                    }</>
+                  }
+                </div>
+              </div>
             </div>
-            <div className="name-and-time">
-              <h2>{statusOwner}</h2>
-              <p>{statusDate}</p>
+            <div className="do-at-status">
+              <div className="button-collect">
+                <div className="button">
+                  <FontAwesomeIcon  icon={["far", "thumbs-up"]} size="1x" color="#4f4f4f"/>
+                  <p>Like</p>
+                  <p>{`( ${likeLength} )`}</p>
+                </div>
+                <div className="button">
+                  <FontAwesomeIcon icon={["far", "comment"]} size="1x" color="#4f4f4f"/>
+                  <p>Comments</p>
+                  <p>{`( ${commentLength} )`}</p>
+                </div>
+                <div className="button">
+                  <FontAwesomeIcon icon={["far", "comments"]} size="1x" color="#4f4f4f"/>
+                  <p>Personal Chat</p>
+                </div>
+              </div>
             </div>
-            <div className="location">
-              <p><img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/location_vBwnULTngQ.png" alt="Location" />{location}</p>
-            </div>
-          </div>
-          <div className="lower-prop">
-            <p>{content}</p>
-            <div className="image-post">
-              {
-                (!media)? <div></div> :
-                <>{
-                  media?.map(media =>(
-                    <div className="image-cont">
-                      <img src={`${media}`} alt="PostMage" />
-                    </div>
-                  ))
-                }</>
-              }
-            </div>
-          </div>
-        </div>
-        <div className="do-at-status">
-          <div className="button-collect">
-            <div className="button">
-              <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/like_DeUkMSVa0GD.png" alt="Like" />
-              <p>Like</p>
-              <p>{`( ${likeLength} )`}</p>
-            </div>
-            <div className="button">
-              <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/comment_pfnyK8aWL.png" alt="Comment" />
-              <p>Comments</p>
-              <p>{`( ${commentLength} )`}</p>
-            </div>
-            <div className="button">
-              <img src="https://ik.imagekit.io/alfianpur/Final_Project/Icon/chat_k1YWihxxc.png" alt="PC" />
-              <p>Personal Chat</p>
-            </div>
-          </div>
-        </div>
+          </>:
+          <p className="no-status-ava">Owner of this status has delete his status</p>
+        }
       </div>
     </div>
   )
