09ed71058d7ebc4d2ccefc2c19ab24a08ac33812
Alfian Alfian <alfianpur@outlook.co.id>
2021-05-31 11:48:12 +0700

    handle media for creating status

--- src/App.js
+++ src/App.js
@@ -18,12 +18,11 @@ import GetAvatar from './pages/GetAvatar';
 import Message from './pages/Message';
 import ProfileSetting from './pages/ProfileSetting'
 import OtherUserPage from './pages/OtherUserPage';
+import SingleStatus from './pages/SingleStatus';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faComments as fasFaComments, faComment as fasFaComment, faThumbsUp as fasFaThumbsUp, faMapMarkerAlt as fasFaMapMarkerAlt, faMapMarkerAlt} from '@fortawesome/free-solid-svg-icons'
 import { faComments as farFaComments, faComment as farFaComment, faThumbsUp as farFaThumbsUp } from '@fortawesome/free-regular-svg-icons'
-import SingleStatus from './pages/SingleStatus';
-
 
 function App() {
 
