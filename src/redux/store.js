b5612b18a3b9e8859d045673664bbf9ad4792438
Alfian Alfian <alfianpur@outlook.co.id>
2021-06-04 15:18:45 +0700

    reduksi logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,7 @@
 import { createStore, applyMiddleware } from  'redux';
-
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk';
-
 import rootReducer from './reducers'
-
-const loggerMiddleware = createLogger()
 
 const devTools =
   process.env.NODE_ENV === "production"
