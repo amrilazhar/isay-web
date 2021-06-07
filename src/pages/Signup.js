6179fec2348a5ce8fc38804c8bdbb8e9bee5bf9b
sm1234sm <septian.maulana@hotmail.com>
2021-06-07 14:43:09 +0700

    Add google authentication implementation

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,71 +1,95 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useLocation } from 'react-router-dom';
-import FlashMessage from '../components/FlashMessage';
-import { alertActions, userActions } from '../redux/actions'
-import { googleClient } from '../helpers/google.client'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
+import FlashMessage from "../components/FlashMessage";
+import { alertActions, userActions } from "../redux/actions";
+import { googleClient } from "../helpers/google.client";
 import { GoogleLogin } from "react-google-login";
-import './style/signup.css'
+import "./style/signup.css";
 
 function Signup() {
-
   const [inputs, setInputs] = useState({
-      email: '',
-      password: '',
-      confirmPassword: ''
+    email: "",
+    password: "",
+    confirmPassword: "",
   });
-  
+
   const { email, password, confirmPassword } = inputs;
   const dispatch = useDispatch();
   const location = useLocation();
 
   useEffect(() => {
-      dispatch(userActions.logout());
+    dispatch(userActions.logout());
   }, []);
 
   function handleChange(e) {
-      const { name, value } = e.target;
-      setInputs(inputs => ({ ...inputs, [name]: value }));
+    const { name, value } = e.target;
+    setInputs((inputs) => ({ ...inputs, [name]: value }));
   }
 
   function handleSubmit(e) {
-      e.preventDefault();
+    e.preventDefault();
 
-      if (email && password && confirmPassword) {
-        const { from } = location.state || { from: { pathname: "/verify" } };
-        dispatch(userActions.register(email, password, confirmPassword, from));
-      } else {
-        dispatch(alertActions.error("Email and password not accepted"));
-      }
+    if (email && password && confirmPassword) {
+      const { from } = location.state || { from: { pathname: "/verify" } };
+      dispatch(userActions.register(email, password, confirmPassword, from));
+    } else {
+      dispatch(alertActions.error("Email and password not accepted"));
+    }
   }
 
-  const alert = useSelector ((state) => state.alert)
-  
+  function handleSubmitGoogle(token) {
+    if (token) {
+      const { from } = location.state || { from: { pathname: "/verify" } };
+      dispatch(userActions.registerGoogle(token.tokenId, from));
+    } else {
+      dispatch(alertActions.error("Google Authentication failed"));
+    }
+  }
+
+  function handleFailureGoogle() {
+    dispatch(alertActions.error("Google Authentication failed"));
+  }
+
+  const alert = useSelector((state) => state.alert);
+
   const responseGoogle = (response) => {
     console.log(response);
-  }
+  };
 
   return (
     <div className="signup-container">
-      {
-        alert.alert ? <FlashMessage/> : ""
-      }
+      {alert.alert ? <FlashMessage /> : ""}
       <div className="signup-wrapper">
         <div className="left-content-signup">
           <div className="logo">
-            <img src="https://i.ibb.co/qsXVjSt/Group-147.png" alt="i say logo" />
+            <img
+              src="https://i.ibb.co/qsXVjSt/Group-147.png"
+              alt="i say logo"
+            />
           </div>
           <h2>See life from a different perspective.</h2>
           <div className="download-button">
-            <Link to ="#"><img src="https://www.pngkit.com/png/full/322-3225520_download-the-app-available-on-the-app-store.png" alt="Download on App Store" /></Link>
-            <Link to ="#"><img src="https://www.talkatone.com/wp-content/themes/talkatone/img/get-app-google-play.png" alt="Get it on Google Play" /></Link>
+            <Link to="#">
+              <img
+                src="https://www.pngkit.com/png/full/322-3225520_download-the-app-available-on-the-app-store.png"
+                alt="Download on App Store"
+              />
+            </Link>
+            <Link to="#">
+              <img
+                src="https://www.talkatone.com/wp-content/themes/talkatone/img/get-app-google-play.png"
+                alt="Get it on Google Play"
+              />
+            </Link>
           </div>
         </div>
         <div className="right-content-signup">
           <div className="login-wrapper">
             <h2>Sign Up</h2>
             <form name="form" onSubmit={handleSubmit}>
-              <label htmlFor="email">Email</label><br />
+              <label htmlFor="email">Email</label>
+              <br />
               <input
                 type="email"
                 name="email"
@@ -73,16 +97,21 @@ function Signup() {
                 placeholder="Type your email"
                 value={email}
                 onChange={handleChange}
-              /><br />
-              <label htmlFor="password">Create a Password</label><br />
-              <input type="password"
+              />
+              <br />
+              <label htmlFor="password">Create a Password</label>
+              <br />
+              <input
+                type="password"
                 name="password"
                 id="password"
                 placeholder="xxxx-xxxx-xxxx"
                 value={password}
                 onChange={handleChange}
-              /><br />
-              <label htmlFor="password">Confirm your Password</label><br />
+              />
+              <br />
+              <label htmlFor="password">Confirm your Password</label>
+              <br />
               <input
                 type="password"
                 name="confirmPassword"
@@ -90,28 +119,37 @@ function Signup() {
                 placeholder="xxxx-xxxx-xxxx"
                 value={confirmPassword}
                 onChange={handleChange}
-              /><br />
-              <input
-                type="submit"
-                value="Create an Account"
               />
-            </form>  
+              <br />
+              <input type="submit" value="Create an Account" />
+            </form>
             <GoogleLogin
               clientId={googleClient.GOOGLE_CLIENT_ID}
-              render={renderProps => (
-                <button onClick={renderProps.onClick} disabled={renderProps.disabled}><img src="https://img.icons8.com/color/50/000000/google-logo.png" alt="Google Logo"/> Signup with Google</button>
+              render={(renderProps) => (
+                <button
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <img
+                    src="https://img.icons8.com/color/50/000000/google-logo.png"
+                    alt="Google Logo"
+                  />{" "}
+                  Signup with Google
+                </button>
               )}
               buttonText="Sign Up"
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
-              cookiePolicy={'single_host_origin'}
+              onSuccess={handleSubmitGoogle}
+              onFailure={handleFailureGoogle}
+              cookiePolicy={"single_host_origin"}
             />
-            <p>Already has an account? <Link to="/login">Login here</Link></p>
+            <p>
+              Already has an account? <Link to="/login">Login here</Link>
+            </p>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
